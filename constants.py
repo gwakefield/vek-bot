4a12c45200bf3d3e76ed1604a72e2605a8f19ac2
Meth962 <ss427mach9@hotmail.com>
2020-03-23 14:00:08 -0500

    Updated statuses
    Changed statuses since Discord incorrectly calls it a 'status' and not a 'game playing'.

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-VEK_STATUSES = ['Bringing Weird to the Masses','Vek Like Things','Chasing a Squirrel, Somewhere','Creeping Out Little Kids','Scaring Children','Getting Lost Somewhere','Still trying to find Geoff in WoW','Eating Pickles','His Rogue, doh, Rogue!','Getting on Burgener\'s Nerves','Trolling Meng']
+VEK_STATUSES = ['Weird Bringer 8.4','Vek Like Games','Squirrel Simulator','Little Kid Creeper','Scaring Children','Lost','WoW, still trying to find Geoff','Pickle Eater','His Rogue, doh, Rogue!','Burgener Aggitator 2.0','Meng Troller 3000']
 VEK_DIRECTIONS = ['Go left, then right, next left, then right...','Check north, then south, then north again, followed by south...','Go up, then down, then back up, then down again..']
 VEK_BODILY_FUNCTIONS = ['BURRRPPPPPP!','burp-Burrrrrrp~!','Braaap!','HIC!','HICCUP!','AAHHHCHOOOOO!']
 VEK_TARGETS = ['Burgener', 'Meng']
